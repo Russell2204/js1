d2defd6549326f0f9108c8066ec492152fda1b51
Russell2204 <77195760+Russell2204@users.noreply.github.com>
2026-03-31 18:24:15 +0500

    Uncommented code for user input and calculations

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-// let name =  prompt("Введите имя...")
-// let surname = prompt("Введите фамилию...")
-// let age = prompt("Введите возраст...")
+let name =  prompt("Введите имя...")
+let surname = prompt("Введите фамилию...")
+let age = prompt("Введите возраст...")
 
-// console.log(`Welcome ${name} ${surname}, your age is ${age}`);
+console.log(`Welcome ${name} ${surname}, your age is ${age}`);
 
-// let num1 = 5
-// let num2 = 5
+let num1 = 5
+let num2 = 5
 
-// alert(num1 + num2)
-// alert(num1 - num2)
-// alert(num1 * num2)
-// alert(num1 / num2)
-// alert(num1 % num2)
+ alert(num1 + num2)
+ alert(num1 - num2)
+ alert(num1 * num2)
+ alert(num1 / num2)
+ alert(num1 % num2)
 
-// let num1 = +prompt('введите первое число')
-// let num2 = +prompt('введите второе число')
+let num1 = +prompt('введите первое число')
+let num2 = +prompt('введите второе число')
 
-// let example1 = +prompt(`${num1} + ${num2} = ?`)
-// console.log(`Пример: ${num1} + ${num2} = ${num1 + num2}, ваш ответ: ${example1}`);
+let example1 = +prompt(`${num1} + ${num2} = ?`)
+ console.log(`Пример: ${num1} + ${num2} = ${num1 + num2}, ваш ответ: ${example1}`);
 
